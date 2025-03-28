001884c24ac510db1639c97dab6c47413765adae
Ben Godfrey <ben@ben2.com>
2025-03-28 10:58:27 +0000

    Backout Astro and enable Treesitter highlighting, re-enable LSP without <Tab> as a completion key

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -216,8 +216,14 @@ require("lazy").setup({
     },
 
     -- syntax highlight EVERYTHING
-    { "nvim-treesitter/nvim-treesitter" },
-    { "sheerun/vim-polyglot" },
+    {
+      "nvim-treesitter/nvim-treesitter".
+      opts = {
+        highlight = {
+          enable = true,
+        },
+      }
+    },
 
     -- highlight/strip trailing whitespace
     { "ntpeters/vim-better-whitespace" },
@@ -227,62 +233,59 @@ require("lazy").setup({
     { "neovim/nvim-lspconfig" },
 
     -- language server management
-    { 'williamboman/mason.nvim' },
-    { 'williamboman/mason-lspconfig.nvim' },
-
-    -- astro lang plugin
-    { 'wuelnerdotexe/vim-astro' },
+    { "williamboman/mason.nvim" },
+    { "williamboman/mason-lspconfig.nvim" },
 
     -- completion + snippets
-    -- {
-    --   "hrsh7th/nvim-cmp",
-    --   event = 'InsertEnter',
-    --   dependencies = {
-    --     { 'L3MON4D3/LuaSnip', version = "v2.*" },
-    --     { 'rafamadriz/friendly-snippets' },
-    --     { "saadparwaiz1/cmp_luasnip", },
-    --     { "hrsh7th/cmp-buffer" },
-    --     { "hrsh7th/cmp-nvim-lsp" },
-    --     { "hrsh7th/cmp-nvim-lua" },
-    --     { "hrsh7th/cmp-path" },
-    --   },
-    --   config = function()
-    --     local cmp = require("cmp")
-    --     local cmp_action = require("lsp-zero").cmp_action()
+    {
+      "hrsh7th/nvim-cmp",
+      event = 'InsertEnter',
+      dependencies = {
+        { 'L3MON4D3/LuaSnip', version = "v2.*" },
+        { 'rafamadriz/friendly-snippets' },
+        { "saadparwaiz1/cmp_luasnip", },
+        { "hrsh7th/cmp-buffer" },
+        { "hrsh7th/cmp-nvim-lsp" },
+        { "hrsh7th/cmp-nvim-lua" },
+        { "hrsh7th/cmp-path" },
+      },
+      config = function()
+        local cmp = require("cmp")
+        local cmp_action = require("lsp-zero").cmp_action()
 
-    --     require("luasnip.loaders.from_vscode").lazy_load()
+        require("luasnip.loaders.from_vscode").lazy_load()
 
-    --     cmp.setup({
-    --       sources = {
-    --         { name = "nvim_lsp" },
-    --         { name = "nvim_lua" },
-    --         { name = "luasnip" },
-    --         { name = "path" },
-    --         { name = "buffer" },
-    --       },
-    --       mapping = cmp.mapping.preset.insert({
-    --         -- `Enter` key to confirm completion
-    --         ["<CR>"] = cmp.mapping.confirm({select = false}),
+        cmp.setup({
+          sources = {
+            { name = "nvim_lsp" },
+            { name = "nvim_lua" },
+            { name = "luasnip" },
+            { name = "path" },
+            { name = "buffer" },
+          },
+          mapping = cmp.mapping.preset.insert({
+            -- `Enter` key to confirm completion
+            ["<CR>"] = cmp.mapping.confirm({select = false}),
 
-    --         -- supertab
-    --         ["<Tab>"] = cmp_action.luasnip_supertab(),
-    --         ["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
+            -- supertab
+            -- ["<Tab>"] = cmp_action.luasnip_supertab(),
+            -- ["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
 
-    --         -- Ctrl+Space to trigger completion menu
-    --         ["<C-Space>"] = cmp.mapping.complete(),
+            -- Ctrl+Space to trigger completion menu
+            ["<C-Space>"] = cmp.mapping.complete(),
 
-    --         -- Navigate between snippet placeholder
-    --         ["<C-f>"] = cmp_action.vim_snippet_jump_forward(),
-    --         ["<C-b>"] = cmp_action.vim_snippet_jump_backward(),
-    --       }),
-    --       snippet = {
-    --         expand = function(args)
-    --           require("luasnip").lsp_expand(args.body)
-    --         end,
-    --       },
-    --     })
-    --   end
-    -- },
+            -- Navigate between snippet placeholder
+            ["<C-f>"] = cmp_action.vim_snippet_jump_forward(),
+            ["<C-b>"] = cmp_action.vim_snippet_jump_backward(),
+          }),
+          snippet = {
+            expand = function(args)
+              require("luasnip").lsp_expand(args.body)
+            end,
+          },
+        })
+      end
+    },
   },
 
   -- Configure any other settings here. See the documentation for more details.
@@ -298,24 +301,24 @@ require("lazy").setup({
 })
 
 -- configure lspzero
--- local lsp_zero = require("lsp-zero")
--- lsp_zero.extend_lspconfig({
---   sign_text = true,
---   lsp_attach = function(client, bufnr)
---     lsp_zero.default_keymaps({ buffer = bufnr })
---   end,
---   capabilities = require("cmp_nvim_lsp").default_capabilities(),
--- })
+local lsp_zero = require("lsp-zero")
+lsp_zero.extend_lspconfig({
+  sign_text = true,
+  lsp_attach = function(client, bufnr)
+    lsp_zero.default_keymaps({ buffer = bufnr })
+  end,
+  capabilities = require("cmp_nvim_lsp").default_capabilities(),
+})
 
--- -- configure mason
--- require("mason").setup({})
--- require("mason-lspconfig").setup({
---   handlers = {
---     function(server_name)
---       require("lspconfig")[server_name].setup({})
---     end,
---   },
--- })
+-- configure mason
+require("mason").setup({})
+require("mason-lspconfig").setup({
+  handlers = {
+    function(server_name)
+      require("lspconfig")[server_name].setup({})
+    end,
+  },
+})
 
 require('onedark').setup {
   style = 'darker'
