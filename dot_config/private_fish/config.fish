c112aeebc33d4861db8605274ff1863287208a9c
Ben Godfrey <ben@ben2.com>
2025-10-23 12:30:31 +0100

    Simplify fish config in case of different Homebrew installation paths

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,14 +1,10 @@
-set -gx HOMEBREW_PREFIX /opt/homebrew
-set -gx HOMEBREW_BIN /opt/homebrew/bin
-set -gx EDITOR $HOMEBREW_BIN/nvim
-
-fish_add_path $HOMEBREW_BIN
+fish_add_path /usr/local/bin
 fish_add_path ~/.local/bin
 fish_add_path ~/.bun/bin
 fish_add_path ~/.cargo/bin
 
-alias vi=$EDITOR
+alias vi=nvim
 
 if status is-interactive
-  set -g fish_greeting
+    set -g fish_greeting
 end
